6015ea066e109723d53f760e9bfd644aa17ef743
maggie serrano <https://github.com/magser110/October2024.git>
2025-02-20 03:11:12 -0500

    saving progress

---
+++ test-jasmine/lib/tests.js
@@ -0,0 +1,29 @@
+describe('Sign up button', () => {
+    let form, successMessage;
+
+    beforeEach(() => {
+        form = { submit: jasmine.createSpy('submit') };
+        successMessage = { style: {display : 'none'}, textContent: ''};
+
+        document.getElementById = jasmine.createSpy('getElementById').and.callFake((id) => {
+            if (id === 'signUpButton') {
+                return {
+                    click: () => {
+                        form.submit();
+                        successMessage.style.display = 'block';
+                        successMessage.textContent = 'Sign up succesful';
+                    },
+                };
+            }
+            return null;
+        });
+    });
+    it('should submit the form and display a success message', () => {
+        const button = document.getElementById('signUpButton');
+        button.click();
+
+        expect(form.submit).toHaveBeenCalled();
+        expect(successMessage.style.display).toBe('block');
+        expect(successMessage.textContent).toBe('Sign up successful');
+    })
+});
